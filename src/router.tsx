925bfcb4f3afb764e7f82b6df4a1bb06cb2fe7cd
kennycha <dudqn136@naver.com>
2025-04-03 22:47:53 +0900

    feat: 404 처리

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,8 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import AdminLoginPage from "./pages/AdminLoginPage";
 import CodeKenDetailPage from "./pages/CodeKenDetailPage";
 import CodeKenListPage from "./pages/CodeKenListPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,14 @@ const router = createBrowserRouter([
   {
     path: "/admin-login",
     element: <AdminLoginPage />,
+  },
+  {
+    path: "/not-found",
+    element: <NotFoundPage />,
+  },
+  {
+    path: "*",
+    element: <Navigate to="/not-found" />,
   },
 ]);
 
